fbb1254e31127b2edcb7bd4bc5e2860a5517c81f
derol12 <61497445+derol12@users.noreply.github.com>
2023-05-07 00:00:46 +0300

    Update my_code.py

--- my_code.py
+++ my_code.py
@@ -1,73 +1,35 @@
 import tensorflow as tf
+from tensorflow.keras import layers
 import numpy as np
+import os
+import time
 import matplotlib.pyplot as plt
+import cv2
 
-# Load the Fashion MNIST dataset
-(train_images, train_labels), (_, _) = tf.keras.datasets.fashion_mnist.load_data()
+# Define the directory where the face images are stored
+data_dir = "D:/img_align_celeba/img_align_celeba"
 
-# Normalize the pixel values to be between -1 and 1
-train_images = (train_images.astype('float32') - 127.5) / 127.5  
+# Define the size of the images
+img_size = (150, 150)
 
-# Define the generator model
-def make_generator_model():
-    model = tf.keras.Sequential()
-    model.add(tf.keras.layers.Dense(7*7*256, use_bias=False, input_shape=(100,)))
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.LeakyReLU())
+# Create an empty list to store the preprocessed images
+data = []
 
-    model.add(tf.keras.layers.Reshape((7, 7, 256)))
-    assert model.output_shape == (None, 7, 7, 256) 
+# Loop through all the files in the data directory
+for filename in os.listdir(data_dir):
+    # Read the image
+    img = cv2.imread(os.path.join(data_dir, filename))
+    # Resize the image to the specified size
+    img = cv2.resize(img, img_size)
+    # Convert the image to grayscale
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Normalize the pixel values to between 0 and 1
+    img = img / 255.0
+    # Add the preprocessed image to the data list
+    data.append(img)
 
-    model.add(tf.keras.layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
-    assert model.output_shape == (None, 7, 7, 128)
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.LeakyReLU())
+# Convert the data list to a NumPy array
+data = np.array(data)
 
-    model.add(tf.keras.layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
-    assert model.output_shape == (None, 14, 14, 64)
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.LeakyReLU())
-
-    model.add(tf.keras.layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
-    assert model.output_shape == (None, 28, 28, 1)
-
-    return model
-
-# Define the discriminator model
-def make_discriminator_model():
-    model = tf.keras.Sequential()
-    model.add(tf.keras.layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
-                                     input_shape=[28, 28, 1]))
-    model.add(tf.keras.layers.LeakyReLU())
-    model.add(tf.keras.layers.Dropout(0.3))
-
-    model.add(tf.keras.layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
-    model.add(tf.keras.layers.LeakyReLU())
-    model.add(tf.keras.layers.Dropout(0.3))
-
-    model.add(tf.keras.layers.Flatten())
-    model.add(tf.keras.layers.Dense(1))
-
-    return model
-
-# Define the loss function for the generator
-def generator_loss(fake_output):
-    return tf.losses.BinaryCrossentropy(from_logits=True)(tf.ones_like(fake_output), fake_output)
-
-# Define the loss function for the discriminator
-def discriminator_loss(real_output, fake_output):
-    real_loss = tf.losses.BinaryCrossentropy(from_logits=True)(tf.ones_like(real_output), real_output)
-    fake_loss = tf.losses.BinaryCrossentropy(from_logits=True)(tf.zeros_like(fake_output), fake_output)
-    total_loss = real_loss + fake_loss
-    return total_loss
-
-# Define the optimizers for the generator and discriminator
-GENERATOR_OPTIMIZER = tf.keras.optimizers.Adam(1e-4)
-DISCRIMINATOR_OPTIMIZER = tf.keras.optimizers.Adam(1e-4)
-
-# Define the batch size and number of training epochs
-BATCH_SIZE = 256
-EPOCHS = 50
-
-# Define the GAN model
-def make_g
+# Save the data array as a NumPy binary file
+np.save("dataset.npy", data)
